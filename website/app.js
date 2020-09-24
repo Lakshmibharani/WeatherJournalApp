318136ecb5e971864583eeadcc57ed2111ea30d4
Lakshmibharani <68168408+Lakshmibharani@users.noreply.github.com>
2020-09-23 21:25:51 -0400

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 let baseURL = "https://api.openweathermap.org/data/2.5/weather?" ;
-let apiKey = "9b789cfb2d097c3887b4d68d5c204ca2";
+let apikey = "9b789cfb2d097c3887b4d68d5c204ca2";
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
